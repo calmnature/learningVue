1febd1377e0a70df9f7f8f794637bf06e67e0196
BestDeveloper <calmnature2@gmail.com>
2025-08-22 08:40:30 +0900

    HTML에 데이터 꽂아넣는 Vue 데이터바인딩 문법

---
+++ vuedongsan/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img src="./assets/logo.svg" alt="Vue logo" width="300px" height="300px">
+  <div>
+    <h4>{{products[0]}}</h4>
+    <p>50 만원</p>
+  </div>
+  <div>
+    <h4>{{products[1]}}</h4>
+    <p>70 만원</p>
+  </div>
+  <div>
+    <h4>{{products[2]}}</h4>
+    <p>90 만원</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name : 'App',
+  data() {
+    return {
+      products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
+    }
+  },
+  components : {
+  }
+}
+</script>
+
+<style>
+</style> 
