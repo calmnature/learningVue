345363bae3814b050a8fc9b2e07cf62c1b5f1dc0
BestDeveloper <calmnature2@gmail.com>
2025-08-22 09:03:13 +0900

    Vue 이벤트 핸들러로 click 감지하기 (허위매물 신고버튼)

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -2,9 +2,24 @@
   <div class="menu">
     <a v-for="(menu, idx) in menus" :key="idx">{{menu}}</a>
   </div>
-  <div v-for="product in products" :key="product">
-    <h4>{{ product }}</h4>
-    <p>50만원</p>
+
+  <div>
+    <h4>{{products[0]}}</h4>
+    <p>50 만원</p>
+    <!-- <button v-on:click="">허위매물신고</button> <span>신고수 : 0</span> -->
+    <!-- v-on: = @ -->
+    <!-- <button @click="declare++">허위매물신고</button> <span>신고수 : {{ declare }}</span> -->
+    <button @click="increase(0)">허위매물신고</button> <span>신고수 : {{ declare[0] }}</span>
+  </div>
+  <div>
+    <h4>{{products[1]}}</h4>
+    <p>70 만원</p>
+    <button @click="increase(1)">허위매물신고</button> <span>신고수 : {{ declare[1] }}</span>
+  </div>
+  <div>
+    <h4>{{products[2]}}</h4>
+    <p>90 만원</p>
+    <button @click="increase(2)">허위매물신고</button> <span>신고수 : {{ declare[2] }}</span>
   </div>
 </template>
 
@@ -13,8 +28,15 @@ export default {
   name : 'App',
   data() {
     return {
+      declare: [0, 0, 0],
       menus: ['Home', 'Shop', 'About'],
       products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      
+    }
+  },
+  methods: {
+    increase(idx) {
+      this.declare[idx]++;
     }
   },
   components : {
