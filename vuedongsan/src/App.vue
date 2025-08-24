45a35003e7610f17596755f31285da075b2682ed
BestDeveloper <calmnature2@gmail.com>
2025-08-25 07:38:05 +0900

    v-if 와 모달창 만들기 (Vue에서 동적인 UI 만드는 법)

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,25 +1,34 @@
 <template>
+  <div class="black-bg" v-if="modalFlag">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+    </div>
+  </div>
   <div class="menu">
     <a v-for="(menu, idx) in menus" :key="idx">{{menu}}</a>
   </div>
 
   <div>
-    <h4>{{products[0]}}</h4>
+    <img src="./assets/room0.jpg" class="room-img">
+    <h4 @click="modalFlag = true">{{products[0]}}</h4>
     <p>50 만원</p>
-    <!-- <button v-on:click="">허위매물신고</button> <span>신고수 : 0</span> -->
-    <!-- v-on: = @ -->
-    <!-- <button @click="declare++">허위매물신고</button> <span>신고수 : {{ declare }}</span> -->
-    <button @click="increase(0)">허위매물신고</button> <span>신고수 : {{ declare[0] }}</span>
+    <button @click="increase(0)">허위매물신고</button>
+    <span>신고수 : {{ declare[0] }}</span>
   </div>
   <div>
+    <img src="./assets/room1.jpg" class="room-img">
     <h4>{{products[1]}}</h4>
     <p>70 만원</p>
-    <button @click="increase(1)">허위매물신고</button> <span>신고수 : {{ declare[1] }}</span>
+    <button @click="increase(1)">허위매물신고</button>
+    <span>신고수 : {{ declare[1] }}</span>
   </div>
   <div>
+    <img src="./assets/room2.jpg" class="room-img">
     <h4>{{products[2]}}</h4>
     <p>90 만원</p>
-    <button @click="increase(2)">허위매물신고</button> <span>신고수 : {{ declare[2] }}</span>
+    <button @click="increase(2)">허위매물신고</button>
+    <span>신고수 : {{ declare[2] }}</span>
   </div>
 </template>
 
@@ -28,6 +37,7 @@ export default {
   name : 'App',
   data() {
     return {
+      modalFlag : false,
       declare: [0, 0, 0],
       menus: ['Home', 'Shop', 'About'],
       products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
@@ -45,6 +55,14 @@ export default {
 </script>
 
 <style>
+  body {
+    margin: 0px;
+  }
+
+  div {
+    box-sizing: border-box;
+  }
+
   #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -62,5 +80,25 @@ export default {
   .menu a {
     color: white;
     padding: 10px;
+  }
+  
+  .room-img{
+    width: 100%;
+    margin-top: 400px;
+  }
+  
+  .black-bg {
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    position: fixed;
+    padding: 20px;
+  }
+
+  .white-bg {
+    width: 100%;
+    background: white;
+    border-radius: 8px;
+    padding: 20px;
   }
 </style> 
