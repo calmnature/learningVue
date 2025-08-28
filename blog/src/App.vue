c80bfb4ce79cab4224cbe2cdde62b4f860ab66ce
BestDeveloper <calmnature2@gmail.com>
2025-08-28 14:30:20 +0900

    블로그 글목록을 만들어보자

--- blog/src/App.vue
+++ blog/src/App.vue
@@ -1,43 +1,62 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown link
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-        </ul>
+  <div>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown link
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
       </div>
+    </nav>
+
+    <div class="container mt-4 text-center">
+      <h4>뷰로그임</h4>
+      <p>- Vue로 만듦 -</p>
+      <List :블로그글="블로그글"/>
     </div>
-  </nav>
+  </div>
 </template>
 
 <script>
-export default {
+import List from './components/List.vue'
+import boardData from './assets/board.js'
 
+export default {
+  name: 'App',
+  data() {
+    return {
+      블로그글 : boardData,
+    }
+  },
+  components: {
+    List,
+  },
 }
 </script>
 
 <style>
 
-</style>>
+</style>
---
+++ blog/src/components/List.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div v-for="(board, idx) in 블로그글" :key="idx">
+            <h5>{{ board.title }}</h5>
+            <p>{{ board.content }}</p>
+            <p>{{ board.date }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'boardList',
+    props: {
+        블로그글: Array,
+    },
+}
+</script>
+
+<style>
+
+</style>
